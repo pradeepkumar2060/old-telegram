66af52883b0320aef26a0cf2a563bae0435b67e4
Ali Rizavi <ali@rizavi.co>
2016-10-24 21:32:30 +0000

    Wiring in Quote class to call QOD and pass it into the rendering layer / template.

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,14 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
+$app->get('/[{category}]', function ($request, $response, $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
+    // Fetch Quote
+    $quoteClient = new \Quote();
+    $quote = $quoteClient->fetchQuote($args['category']);
+
     // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+    return $this->renderer->render($response, 'index.phtml', array("quote" => $quote) );
 });
